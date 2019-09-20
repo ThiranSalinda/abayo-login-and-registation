6dad1b493c719a4fa6a8962a39bc11df9a070d8c
thiran <thiransalinda.i@gmail.com>
2019-09-20 13:17:41 +0530

    abayo login and registation2

--- app/src/main/java/com/tx/abayo/MainActivity.java
+++ app/src/main/java/com/tx/abayo/MainActivity.java
@@ -2,13 +2,40 @@ package com.tx.abayo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignup,btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignup=(Button) findViewById(R.id.btnSignup);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btnSignup=(Button) findViewById(R.id.btnLogin);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,SignupActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
